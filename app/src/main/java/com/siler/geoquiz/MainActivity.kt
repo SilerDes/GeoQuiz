f42337581b269638241c94fa4a7fcaca128b4402
Andrew Kazbekov <ninjaxs2or@gmail.com>
2021-12-24 02:57:00 +0300

    Project version 1.0 Developed MainActivity, activity_main and string resources

--- app/src/main/java/com/siler/geoquiz/MainActivity.kt
+++ app/src/main/java/com/siler/geoquiz/MainActivity.kt
@@ -2,10 +2,40 @@ package com.siler.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.trueButton)
+        falseButton = findViewById(R.id.falseButton)
+
+        trueButton.setOnClickListener {
+            toastBottom(R.string.correct_toast)
+        }
+
+        falseButton.setOnClickListener {
+            toastBottom(R.string.incorrect_toast)
+        }
+    }
+
+    private fun toastTop(resId: Int) {
+        val toast = Toast.makeText(this, resId, Toast.LENGTH_SHORT)
+        toast.apply {
+            setGravity(Gravity.TOP,0,0)
+            show()
+        }
+    }
+
+    private fun toastBottom(resId: Int) {
+        Toast.makeText(this, resId, Toast.LENGTH_SHORT).show()
     }
 }
